0fe72f93bffd9506eae083f6ed6ed1b8f6807e4e
folkmanis <folkmanis@gmail.com>
2023-07-31 23:55:50 +0300

    strict mode enabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Calculator } from './scripts/components/Calculator';
 import './styles.css';
+import { StrictMode } from 'react';
 
 export function App() {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center p-2">
-      <Calculator />
+      <StrictMode>
+        <Calculator />
+      </StrictMode>
     </div>
   );
 }
