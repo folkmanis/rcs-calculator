cc059fc9511078daa112a24a92b7856279598f8e
folkmanis <folkmanis@gmail.com>
2023-08-12 15:45:34 +0300

    refactored Ripple

--- src/scripts/components/Ripple.tsx
+++ src/scripts/components/Ripple.tsx
@@ -2,30 +2,21 @@ import { useRef } from 'react';
 import '../../styles/ripple.css';
 import { RippleStyle } from '../types/ripple-style';
 import { ActiveRipple } from '../types/active-ripple';
+import { calculateRipplePosition } from '../utilities/calculate-ripple-position';
 
 interface RippleProps {
   ripples: ActiveRipple[];
 }
 
 export function Ripple({ ripples }: RippleProps) {
-  const rippleArray: RippleStyle[] = [];
   const containerRef = useRef<HTMLDivElement>(null);
 
-  const element = containerRef.current?.getBoundingClientRect();
-  if (element !== undefined) {
-    const diameter = Math.max(element.width, element.height);
-    const radius = diameter / 2;
-    ripples.forEach(ripple => {
-      const clientX = ripple.clientX ?? element.x + element.width / 2;
-      const clientY = ripple.clientY ?? element.y + element.height / 2;
-      const newRipple: RippleStyle = {
-        width: `${diameter}px`,
-        height: `${diameter}px`,
-        left: `${clientX - (element.x + radius)}px`,
-        top: `${clientY - (element.y + radius)}px`,
-      };
-      rippleArray.push(newRipple);
-    });
+  let rippleArray: RippleStyle[] = [];
+  if (containerRef.current !== null) {
+    const element = containerRef.current.getBoundingClientRect();
+    rippleArray = ripples.map(ripple =>
+      calculateRipplePosition(element, ripple)
+    );
   }
 
   return (
---
+++ src/scripts/utilities/calculate-ripple-position.ts
@@ -0,0 +1,18 @@
+import { ActiveRipple } from '../types/active-ripple';
+import { RippleStyle } from '../types/ripple-style';
+
+export function calculateRipplePosition(
+    container: DOMRect,
+    ripple: ActiveRipple
+): RippleStyle {
+    const diameter = Math.max(container.width, container.height);
+    const radius = diameter / 2;
+    const clientX = ripple.clientX ?? container.x + container.width / 2;
+    const clientY = ripple.clientY ?? container.y + container.height / 2;
+    return {
+        width: `${diameter}px`,
+        height: `${diameter}px`,
+        left: `${clientX - (container.x + radius)}px`,
+        top: `${clientY - (container.y + radius)}px`,
+    };
+}
